8c76803bf64060b7711aeadb4fd4dc8b3ed7c6ad
moniqueperez1 <143114132+moniqueperez1@users.noreply.github.com>
2025-05-04 18:31:12 -0500

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -41,7 +41,7 @@ public class Utility extends Thread {
 
 	public static void log(String format, Object ... args) {
 		synchronized(fileLock) {
-			try(PrintWriter out = new PrintWriter(new FileWriter("/logs/shared-log.txt", true))){
+			try(PrintWriter out = new PrintWriter(new FileWriter("/logs/log.txt", true))){
 				out.printf("%s [%s] [Utility] ", java.time.LocalDateTime.now(), Thread.currentThread().getName());
 				out.printf(format + "%n", args);
 			}catch(IOException e) {
